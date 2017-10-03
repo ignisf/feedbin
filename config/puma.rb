fb8ce7f432aef7b15acd9eeeba8995d1105d53cc
Petko Bordjukov <bordjukov@gmail.com>
2017-10-03 15:51:27 +0300

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,11 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+if ENV['SOCKET'] then
+  bind 'unix://' + ENV['SOCKET']
+else
+  port ENV.fetch('PORT') { 3000 }
+end
 
 # Specifies the `environment` that Puma will run in.
 #
